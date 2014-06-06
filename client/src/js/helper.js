df97689f4b7bfb2ef7f40ffaa7202bf4e313960f
ybouhjira <b.youssef91@gmail.com>
2014-06-06 17:14:55 +0100

    A shorter version of the random color generator

--- client/src/js/helper.js
+++ client/src/js/helper.js
@@ -12,13 +12,7 @@
     return {
       // Return a random color in hex notation
       getRandomColor: function() {
-        var letters = '0123456789ABCDEF'.split('')
-          , color = '#';
-
-        for (var i = 0; i < 6; i++ ) 
-            color += letters[Math.floor(Math.random() * 16)];
-
-        return color;
+        return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
       }
     };
   });
@@ -30,38 +24,4 @@
     }
   });
 
-  app.filter('smoothen', function() {
-    return function(path) {
-      var lats = path.map(function(pos) {
-        return pos.latitude;
-      });
-
-      var lons = path.map(function(pos) {
-        return pos.longitude;
-      });
-
-      return (function bspline(lats, lons) {
-        var i, t, ax, ay, bx, by, cx, cy, dx, dy, lat, lon, points;
-        points = [];
-        
-        // For every point
-        for (i = 2; i < lats.length - 2; i++) {
-          for (t = 0; t < 1; t += 0.2) {
-              ax = (-lats[i - 2] + 3 * lats[i - 1] - 3 * lats[i] + lats[i + 1]) / 6;
-              ay = (-lons[i - 2] + 3 * lons[i - 1] - 3 * lons[i] + lons[i + 1]) / 6;
-              bx = (lats[i - 2] - 2 * lats[i - 1] + lats[i]) / 2;
-              by = (lons[i - 2] - 2 * lons[i - 1] + lons[i]) / 2;
-              cx = (-lats[i - 2] + lats[i]) / 2;
-              cy = (-lons[i - 2] + lons[i]) / 2;
-              dx = (lats[i - 2] + 4 * lats[i - 1] + lats[i]) / 6;
-              dy = (lons[i - 2] + 4 * lons[i - 1] + lons[i]) / 6;
-              lat = ax * Math.pow(t + 0.1, 3) + bx * Math.pow(t + 0.1, 2) + cx * (t + 0.1) + dx;
-              lon = ay * Math.pow(t + 0.1, 3) + by * Math.pow(t + 0.1, 2) + cy * (t + 0.1) + dy;
-              points.push(new google.maps.LatLng(lat, lon));
-          }
-        }
-        return points;
-      })(lats, lons);
-    }
-  });
 })();
